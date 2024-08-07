c214e0509a82951317fdb3d6c56c3faea2ba912a
Rishit Sinha <rishitsinha07@gmail.com>
2024-08-08 04:00:00 +0530

    Pass first two tests
    The code passes the first 2 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function(array, ...numbers) {
+    let inputArray = array;
+    let removeNumArray = [];
+    for(const num of numbers){
+        let i = 0;
+        for(const integer of inputArray){
+            if(integer === num){
+                removeNumArray.push(i);
+            }
+            i++;
+        }
+    }
+    for(const j of removeNumArray){
+        inputArray.splice(j, 1);
+    }
+    return inputArray;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
