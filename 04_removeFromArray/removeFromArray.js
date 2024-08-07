7a3099ee134aa7c2170fa1d6e86e0a3d739cbaa3
Rishit Sinha <rishitsinha07@gmail.com>
2024-08-08 04:21:42 +0530

    Pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,16 @@
 const removeFromArray = function(array, ...numbers) {
     let inputArray = array;
-    let removeNumArray = [];
     for(const num of numbers){
         let i = 0;
         for(const integer of inputArray){
             if(integer === num){
                 inputArray.splice(i, 1);
+                i--;
+                numbers.push(num)
             }
             i++;
         }
     }
-    // for(const j of removeNumArray){
-    //     inputArray.splice(j, 1);
-    // }
     return inputArray;
 }
 // Do not edit below this line
