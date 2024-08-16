b21a30a556a9907efaea895c354aa07e201698a2
Rishit Sinha <rishitsinha07@gmail.com>
2024-08-17 01:29:51 +0530

    All functions other than factorial work

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,24 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, sum) => acc + sum, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, product) => acc * product, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
+const factorial = function(num) {
 	
 };
 
